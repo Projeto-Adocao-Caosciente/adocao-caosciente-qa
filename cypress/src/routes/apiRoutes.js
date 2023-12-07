bab57642111737cd0e16d02f0121e3a81007e939
João Lucas Pereira <j.lucaspereira@usp.br>
2023-12-07 19:45:28 -0300

    feat: created file for the ApiRoutes

---
+++ cypress/src/routes/apiRoutes.js
@@ -0,0 +1,39 @@
+const ApiRoutes = {
+  register: {
+    ong: "/auth/register_ong",
+    adopter: "/auth/register_adopter",
+  },
+
+  authentication: "/auth/login",
+
+  ong: {
+    pet: {
+      getAll: "/ong/animals",
+      getOne: "/ong/animals/**",
+      search: "/ong/animals?name=**",
+      register: "/ong/animals",
+
+      form: {
+        getAll: "/ong/animals/**/forms",
+        getOne: "/ong/animals/**/forms/**",
+        register: "/ong/animals/**/forms",
+      },
+    },
+
+    details: "/auth/profile",
+    edit: "/ong",
+  },
+
+  adopter: {
+    pet: {
+      getAll: "/adopter/animals",
+      getOne: "/adopter/animals/**",
+      search: "/adopter/animals?name=**",
+    },
+
+    details: "/auth/profile",
+    edit: "/adopter",
+  },
+};
+
+export { ApiRoutes };
