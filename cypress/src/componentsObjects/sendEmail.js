f5a03f443576275861178f5cfe84f3f8042caebf
João Lucas Pereira <j.lucaspereira@usp.br>
2023-12-10 17:07:04 -0300

    added more commands for the send email component

--- cypress/src/componentsObjects/sendEmail.js
+++ cypress/src/componentsObjects/sendEmail.js
@@ -16,6 +16,8 @@ class sendEmail {
     this.emailsToSend.forEach((email) => {
       this._addRecipient(email);
     });
+
+    this.sendEmail();
   }
 
   _addOneRecipient(email) {
@@ -32,9 +34,21 @@ class sendEmail {
   clickAddEmailButton() {
     cy.get(this.locatorsSendEmail.addEmailButton).click();
   }
-  
+
   removeRecipient(email) {
-    cy.get(this.locatorsSendEmail.listOfRecipients(email).deleteRecipient).click();
+    cy.get(
+      this.locatorsSendEmail.listOfRecipients(email).deleteRecipient
+    ).click();
+  }
+
+  sendEmail() {
+    cy.get(this.locatorsSendEmail.navigation.sendEmailButton).click();
+
+    this._checksIfTheEmailsWereSent();
+  }
+
+  cancelSendEmail() {
+    cy.get(this.locatorsSendEmail.navigation.backButton).click();
   }
 
   _checksIfTheRecipientWasAdded(email) {
@@ -43,6 +57,19 @@ class sendEmail {
 
     cy.elementExpected(
       this.locatorsSendEmail.listOfRecipients(email).user,
+      "should",
+      "exist",
+      customSuccessMessage,
+      customFailureMessage
+    );
+  }
+
+  _checksIfTheEmailsWereSent() {
+    const customSuccessMessage = `[DialogSendEmail] Os e-mails foram enviados com sucesso.`;
+    const customFailureMessage = `[DialogSendEmail] Houve um erro ao enviar os e-mails.`;
+
+    cy.elementExpected(
+      locatorsToast.success,
       "should",
       "exist",
       customSuccessMessage,
